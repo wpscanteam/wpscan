8b0558063ed9a6c9eef937a92a27f2588cff0d34
Christian Mehlmauer <firefart@gmail.com>
2012-09-13 00:06:50 +0200

    generate list of popular or all themes

--- wpstools.rb
+++ wpstools.rb
@@ -38,8 +38,12 @@ begin
     ["--verbose", "-v", GetoptLong::NO_ARGUMENT],
     ["--generate_plugin_list", GetoptLong::OPTIONAL_ARGUMENT],
     ["--generate_full_plugin_list", GetoptLong::NO_ARGUMENT],
+    ["--generate_theme_list", GetoptLong::OPTIONAL_ARGUMENT],
+    ["--generate_full_theme_list", GetoptLong::NO_ARGUMENT],
     ["--gpl", GetoptLong::OPTIONAL_ARGUMENT],
     ["--gfpl", GetoptLong::OPTIONAL_ARGUMENT],
+    ["--gtl", GetoptLong::OPTIONAL_ARGUMENT],
+    ["--gftl", GetoptLong::OPTIONAL_ARGUMENT],
     ["--update", "-u", GetoptLong::NO_ARGUMENT]
   )
 
@@ -59,23 +63,46 @@ begin
       end
 
       @generate_plugin_list = true
+    when "--generate_theme_list", "--gtl"
+      if argument == ''
+        puts "Number of pages not supplied, defaulting to 150 pages ..."
+        @number_of_pages = 150
+        else
+        @number_of_pages = argument.to_i
+      end
+      
+      @generate_theme_list = true
     when "--update"
       @update = true
     when "--generate_full_plugin_list", "--gfpl"
       @generate_full_plugin_list = true
+    when "--generate_full_theme_list", "--gftl"
+      @generate_full_theme_list = true
     end
   end
 
   if @generate_plugin_list
     puts "[+] Generating new most popular plugin list"
     puts
-    Generate_Plugin_List.new(@number_of_pages, @verbose).save_file(false)
+    Generate_List.new('plugins', @verbose).generate_popular_list(@number_of_pages)
   end
     
   if @generate_full_plugin_list
     puts "[+] Generating new full plugin list"
     puts
-    Generate_Plugin_List.new(-1, @verbose).save_file(true)
+    Generate_List.new('plugins', @verbose).generate_full_list
+  end
+  
+  if @generate_theme_list
+    puts "[+] Generating new most popular theme list"
+    puts
+    Generate_List.new('themes', @verbose).generate_popular_list(@number_of_pages)
+  end
+  
+  if @generate_full_theme_list
+    puts "[+] Generating new full theme list"
+    puts
+    Generate_List.new('themes', @verbose).generate_full_list
   end
 
   if @update
@@ -88,6 +115,7 @@ begin
   end
 
 rescue => e
-  puts "[ERROR] #{e}"
-  puts "Trace : #{e.backtrace}"
+  puts "[ERROR] #{e.message}"
+  puts "Trace :"
+  puts e.backtrace.join("\n")
 end
