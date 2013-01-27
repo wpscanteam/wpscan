79b5490676fe8b67abffbe6f74c12917afd13d83
ethicalhack3r <ryandewhurst@gmail.com>
2013-01-27 12:41:51 +0100

    Changed if to unless.

--- lib/cache_file_store.rb
+++ lib/cache_file_store.rb
@@ -47,7 +47,7 @@ class CacheFileStore
 
   def clean
     Dir[File.join(@storage_path, '*')].each do |f|
-      File.delete(f) if !File.symlink?(f)
+      File.delete(f) unless File.symlink?(f)
     end
   end
 
