3ce42b641ccfd5f654483b44f4f8c24c554c255b
erwanlr <erwan.lr@gmail.com>
2013-04-05 14:06:31 +0200

    License text removed from sources, see the LICENSE file

--- wpstools.rb
+++ wpstools.rb
@@ -1,24 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
-#--
-# WPScan - WordPress Security Scanner
-# Copyright (C) 2012-2013
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#++
-
 $: << '.'
 require File.dirname(__FILE__) + '/lib/wpstools/wpstools_helper'
 
