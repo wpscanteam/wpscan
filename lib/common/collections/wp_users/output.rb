5acaff356fe6e0c39d2719c491983abbd0ae7a7d
erwanlr <erwan.lr@gmail.com>
2013-04-13 22:37:16 +0200

    Indentation fix

--- lib/common/collections/wp_users/output.rb
+++ lib/common/collections/wp_users/output.rb
@@ -3,26 +3,26 @@
 class WpUsers < WpItems
   module Output
 
-  # @param [ Hash ] options
-  # @option options[ Boolean ] :show_password Output the password column
-  #
-  # @return [ void ]
-  def output(options = {})
+    # @param [ Hash ] options
+    # @option options[ Boolean ] :show_password Output the password column
+    #
+    # @return [ void ]
+    def output(options = {})
 
-    rows     = []
-    headings = ['Id', 'Name']
-    headings << 'Password' if options[:show_password]
+      rows     = []
+      headings = ['Id', 'Name']
+      headings << 'Password' if options[:show_password]
 
-    self.each do |wp_user|
-      row = [wp_user.id, wp_user.display_name]
-      row << wp_user.password if options[:show_password]
-      rows << row
+      self.each do |wp_user|
+        row = [wp_user.id, wp_user.display_name]
+        row << wp_user.password if options[:show_password]
+        rows << row
+      end
+
+      puts Terminal::Table.new(headings: headings,
+                               rows: rows,
+                               style: { margin_left: options[:margin_left] || '' })
     end
-
-    puts Terminal::Table.new(headings: headings,
-                             rows: rows,
-                             style: { margin_left: options[:margin_left] || '' })
-  end
 
   end
 end
