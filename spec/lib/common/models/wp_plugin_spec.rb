a032b7c134b09181f331fde11b68e5e9b7053316
Christian Mehlmauer <firefart@gmail.com>
2013-08-24 11:16:39 +0200

    more reference tags, fixes issue #268

--- spec/lib/common/models/wp_plugin_spec.rb
+++ spec/lib/common/models/wp_plugin_spec.rb
@@ -7,7 +7,15 @@ describe WpPlugin do
   it_behaves_like 'WpItem::Vulnerable' do
     let(:options)        { { name: 'white-rabbit' } }
     let(:vulns_file)     { MODELS_FIXTURES + '/wp_plugin/vulnerable/plugins_vulns.xml' }
-    let(:expected_vulns) { Vulnerabilities.new << Vulnerability.new('Follow me!', 'REDIRECT', ['http://ref2.com']) }
+		let(:expected_refs)  { {
+				:url => ['Ref 1', 'Ref 2'],
+				:cve => ['2011-001'],
+				:secunia => ['secunia'],
+				:osvdb => ['osvdb'],
+				:metasploit => ['exploit/ex1'],
+				:exploitdb => ['exploitdb']
+		} }
+    let(:expected_vulns) { Vulnerabilities.new << Vulnerability.new('Follow me!', 'REDIRECT', expected_refs) }
   end
 
   subject(:wp_plugin) { WpPlugin.new(uri, options) }
