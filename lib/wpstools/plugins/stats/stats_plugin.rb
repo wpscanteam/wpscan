99e4e5a219202d9119f4aff4ad722fa9175a85e8
Peter <vanderlaan.pm@gmail.com>
2014-01-07 14:00:08 +0100

    Added database modification dates in status report

--- lib/wpstools/plugins/stats/stats_plugin.rb
+++ lib/wpstools/plugins/stats/stats_plugin.rb
@@ -12,18 +12,34 @@ class StatsPlugin < Plugin
 
   def run(options = {})
     if options[:stats]
+      date_wp = File.mtime(WP_VULNS_FILE)
+      date_plugins = File.mtime(PLUGINS_VULNS_FILE)
+      date_themes = File.mtime(THEMES_VULNS_FILE)
+      date_plugins_full = File.mtime(PLUGINS_FULL_FILE)
+      date_themes_full = File.mtime(THEMES_FULL_FILE)
+
       puts "WPScan Database Statistics:"
       puts "---------------------------"
       puts "[#] Total WordPress Sites in the World: #{get_wp_installations}"
-      puts "[#] Total vulnerable versions: #{vuln_core_count}"
-      puts "[#] Total vulnerable plugins: #{vuln_plugin_count}"
-      puts "[#] Total vulnerable themes: #{vuln_theme_count}"
-      puts "[#] Total version vulnerabilities: #{version_vulns_count}"
-      puts "[#] Total plugin vulnerabilities: #{plugin_vulns_count}"
-      puts "[#] Total theme vulnerabilities: #{theme_vulns_count}"
-      puts "[#] Total plugins to enumerate: #{total_plugins}"
-      puts "[#] Total themes to enumerate: #{total_themes}"
       puts
+      puts "[#] Total vulnerable versions: #{vuln_core_count}"
+      puts "[#] Total vulnerable plugins:  #{vuln_plugin_count}"
+      puts "[#] Total vulnerable themes:   #{vuln_theme_count}"
+      puts
+      puts "[#] Total version vulnerabilities: #{version_vulns_count}"
+      puts "[#] Total plugin vulnerabilities:  #{plugin_vulns_count}"
+      puts "[#] Total theme vulnerabilities:   #{theme_vulns_count}"
+      puts
+      puts "[#] Total plugins to enumerate:  #{total_plugins}"
+      puts "[#] Total themes to enumerate:   #{total_themes}"
+      puts
+      puts "[+] WordPress DB modified: #{date_wp.strftime('%Y-%m-%d %H:%M:%S')}"
+      puts "[+] Plugins DB modified:   #{date_plugins.strftime('%Y-%m-%d %H:%M:%S')}"
+      puts "[+] Themes DB modified:    #{date_themes.strftime('%Y-%m-%d %H:%M:%S')}"
+      puts "[+] Enumeration plugins:   #{date_plugins_full.strftime('%Y-%m-%d %H:%M:%S')}"
+      puts "[+] Enumeration themes:    #{date_themes_full.strftime('%Y-%m-%d %H:%M:%S')}"
+      puts
+      puts "[+] Report generated:      #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
     end
   end
 
