c24ee89b16a2b9e02f871df932cbcd90ebb014d7
Peter <vanderlaan.pm@gmail.com>
2014-01-08 23:34:35 +0100

    Some modifications in the output of an item

--- lib/common/models/wp_item/output.rb
+++ lib/common/models/wp_item/output.rb
@@ -9,9 +9,11 @@ class WpItem
       puts " | Name: #{self}" #this will also output the version number if detected
       puts " | Location: #{url}"
       #puts " | WordPress: #{wordpress_url}" if wordpress_org_item?
-      puts ' | Directory listing enabled: Yes' if has_directory_listing?
       puts " | Readme: #{readme_url}" if has_readme?
       puts " | Changelog: #{changelog_url}" if has_changelog?
+      puts " |"
+      puts " | " + red('[!]') + " Directory listing is enabled: #{url}" if has_directory_listing?
+      puts " | " + red('[!]') + " An error_log file has been found: #{error_log_url}" if has_error_log?
 
       if respond_to?(:additional_output)
         additional_output(verbose)
@@ -19,9 +21,6 @@ class WpItem
 
       vulnerabilities.output
 
-      if has_error_log?
-        puts ' | ' + red('[!]') + " An error_log file has been found : #{error_log_url}"
-      end
     end
 
   end
