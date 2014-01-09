76a6d59837f28984c97b8e1c30539e00e643f157
Peter <vanderlaan.pm@gmail.com>
2014-01-09 10:24:40 +0100

    Output cosmetics

--- lib/common/models/wp_version/output.rb
+++ lib/common/models/wp_version/output.rb
@@ -4,13 +4,13 @@ class WpVersion < WpItem
   module Output
 
     def output(verbose = false)
+      puts
       puts green('[+]') + " WordPress version #{self.number} identified from #{self.found_from}"
 
       vulnerabilities = self.vulnerabilities
 
       unless vulnerabilities.empty?
-        puts
-        puts red('[!]') + " #{vulnerabilities.size} vulnerabilities identified from the version number:"
+        puts red('[!]') + " #{vulnerabilities.size} vulnerabilities identified from the version number"
 
         vulnerabilities.output
       end
