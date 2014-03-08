ec5d12c940e798f0cd801ff26120fdd73dc1aef6
Peter <vanderlaan.pm@gmail.com>
2014-03-08 22:47:43 +0100

    Changed layout and coloring

--- lib/common/models/wp_item/output.rb
+++ lib/common/models/wp_item/output.rb
@@ -6,13 +6,13 @@ class WpItem
     # @return [ Void ]
     def output(verbose = false)
       puts
-      puts " | Name: #{self}" #this will also output the version number if detected
-      puts " | Location: #{url}"
+      puts "#{green('[+]')} Name: #{self}" #this will also output the version number if detected
+      puts " |  Location: #{url}"
       #puts " | WordPress: #{wordpress_url}" if wordpress_org_item?
-      puts " | Readme: #{readme_url}" if has_readme?
-      puts " | Changelog: #{changelog_url}" if has_changelog?
-      puts " | " + red('[!]') + " Directory listing is enabled: #{url}" if has_directory_listing?
-      puts " | " + red('[!]') + " An error_log file has been found: #{error_log_url}" if has_error_log?
+      puts " |  Readme: #{readme_url}" if has_readme?
+      puts " |  Changelog: #{changelog_url}" if has_changelog?
+      puts "#{red('[!]')} Directory listing is enabled: #{url}" if has_directory_listing?
+      puts "#{red('[!]')} An error_log file has been found: #{error_log_url}" if has_error_log?
 
       additional_output(verbose) if respond_to?(:additional_output)
 
