04b1cee71eefd0108585ecfab90ebda46d3b2957
erwanlr <erwan.lr@gmail.com>
2014-09-02 17:12:56 +0200

    Factorises a crappy spec - Kudos to @hlissner for this one ;)

--- spec/lib/common/collections/wp_items_spec.rb
+++ spec/lib/common/collections/wp_items_spec.rb
@@ -10,29 +10,15 @@ describe WpItems do
 
     let(:expected) do
       {
-        request_params:                 { cache_ttl: 0, followlocation: true },
-        targets_items_from_file:        [ WpItem.new(uri, name: 'item1'),
-                                          WpItem.new(uri, name:'item-2'),
-                                          WpItem.new(uri, name: 'mr-smith')],
+        request_params:           { cache_ttl: 0, followlocation: true },
+        targets_items_from_file:  [ WpItem.new(uri, name: 'item1'),
+                                    WpItem.new(uri, name: 'item-2'),
+                                    WpItem.new(uri, name: 'mr-smith')],
 
-        vulnerable_targets_items:       [ WpItem.new(uri, name: 'mr-smith'),
-                                          WpItem.new(uri, name: 'neo')],
+        vulnerable_targets_items: [ WpItem.new(uri, name: 'mr-smith'),
+                                    WpItem.new(uri, name: 'neo')],
 
-        # Any better way to do this ? :x
-        passive_detection: WpItems.new << WpItem.new(uri, name: 'detect-me-1') <<
-                                          WpItem.new(uri, name: 'detect-me-2') <<
-                                          WpItem.new(uri, name: 'detect-me-3') <<
-                                          WpItem.new(uri, name: 'detect-me-4') <<
-                                          WpItem.new(uri, name: 'detect-me-5') <<
-                                          WpItem.new(uri, name: 'detect-me-6') <<
-                                          WpItem.new(uri, name: 'detect-me-7') <<
-                                          WpItem.new(uri, name: 'detect-me-8') <<
-                                          WpItem.new(uri, name: 'detect-me-9') <<
-                                          WpItem.new(uri, name: 'detect-me-10') <<
-                                          WpItem.new(uri, name: 'detect-me-11') <<
-                                          WpItem.new(uri, name: 'detect-me-12') <<
-                                          WpItem.new(uri, name: 'detect-me-13') <<
-                                          WpItem.new(uri, name: 'detect-me-14')
+        passive_detection: (1..14).reduce(WpItems.new) { |o, i| o << WpItem.new(uri, name: "detect-me-#{i}") }
       }
     end
   end
