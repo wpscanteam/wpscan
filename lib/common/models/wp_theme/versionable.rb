e3ac331a712507089ac4a131ac866da5fc4cadfb
erwanlr <erwan.lr@gmail.com>
2014-12-10 17:01:14 +0100

    Removes the theme version check from the readme, unrealistic scenario - Ref #737

--- lib/common/models/wp_theme/versionable.rb
+++ lib/common/models/wp_theme/versionable.rb
@@ -2,16 +2,8 @@
 
 class WpTheme < WpItem
   module Versionable
-
     def version
-      unless @version
-        @version = Browser.get(style_url).body[%r{Version:\s*([^\s]+)}i, 1]
-
-        # Get Version from readme.txt
-        @version ||= super
-      end
-      @version
+      @version ||= Browser.get(style_url).body[%r{Version:\s*([^\s]+)}i, 1]
     end
-
   end
 end
--- spec/shared_examples/wp_theme_versionable.rb
+++ spec/shared_examples/wp_theme_versionable.rb
@@ -21,26 +21,9 @@ shared_examples 'WpTheme::Versionable' do
     end
 
     context 'when the version is not found' do
-      let(:file)       { 'twentyeleven-unknow.css' }
-      let(:readme_url) { subject.uri.merge('readme.txt').to_s }
-
-      context 'from the style_url' do
-        it 'gets it from the readme' do
-          stub_request(:get, readme_url).to_return(status: 200, body: 'Stable Tag: 1.3.4')
-
-          @file     = file
-          @expected = '1.3.4'
-        end
-      end
-
-      context 'from both style & readme' do
-        it 'returns nil' do
-          allow(subject).to receive_messages(readme_url: readme_url)
-          stub_request(:get, readme_url).to_return(status: 404)
-
-          @file     = file
-          @expected = nil
-        end
+      it 'returns nil' do
+        @file     = 'twentyeleven-unknow.css'
+        @expected = nil
       end
     end
 
