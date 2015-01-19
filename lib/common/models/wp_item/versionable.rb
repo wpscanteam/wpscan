507cf1d5116665a6f800cf30e4734659826be3e3
Christian Mehlmauer <firefart@gmail.com>
2015-01-19 23:41:51 +0100

    fix regex

--- lib/common/models/wp_item/versionable.rb
+++ lib/common/models/wp_item/versionable.rb
@@ -13,9 +13,9 @@ class WpItem
         # This check is needed because readme_url can return nil
         if has_readme?
           response = Browser.get(readme_url)
-          version = response.body[/(?:stable tag|version):\s*(?!trunk)([0-9a-z.-]+)/i, 1]
+          version = response.body[/(?:stable tag|version):\s*(?!trunk)([0-9a-z\.-]+)/i, 1]
           if version.nil?
-            extracted_versions = response.body.scan(/[=]+\s*([0-9.-]+)\s*[=]+/i)
+            extracted_versions = response.body.scan(/[=]+\s*([0-9\.-]+)\s*[=]+/i)
             return if extracted_versions.nil? || extracted_versions.length == 0
             sorted = extracted_versions.flatten.sort { |x,y| Gem::Version.new(x) <=> Gem::Version.new(y) }
             @version = sorted.last
