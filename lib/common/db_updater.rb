41ae47f065cf631fac87079fd228db31f69e5861
Christian Mehlmauer <firefart@gmail.com>
2015-01-21 21:53:54 +0100

    sync license

--- lib/common/db_updater.rb
+++ lib/common/db_updater.rb
@@ -6,7 +6,7 @@ class DbUpdater
     local_vulnerable_files.xml local_vulnerable_files.xsd
     plugins_full.txt plugins.txt themes_full.txt themes.txt
     timthumbs.txt user-agents.txt wp_versions.xml wp_versions.xsd
-    plugin_vulns.json theme_vulns.json wp_vulns.json
+    plugin_vulns.json theme_vulns.json wp_vulns.json LICENSE
   )
 
   attr_reader :repo_directory
