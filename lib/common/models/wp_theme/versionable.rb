0d806e6d74a5e34bb47dd1a5591980dc313f20cb
erwanlr <erwan.lr@gmail.com>
2015-05-01 09:56:18 +0100

    Ignores potential non version chars in theme version detection - Fixes #816

--- lib/common/models/wp_theme/versionable.rb
+++ lib/common/models/wp_theme/versionable.rb
@@ -3,7 +3,7 @@
 class WpTheme < WpItem
   module Versionable
     def version
-      @version ||= Browser.get(style_url).body[%r{Version:\s*([^\s]+)}i, 1]
+      @version ||= Browser.get(style_url).body[%r{Version:\s*(?!trunk)([0-9a-z\.-]+)}i, 1]
     end
   end
 end
