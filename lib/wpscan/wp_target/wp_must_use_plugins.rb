e7dbf9278d3a148134b189ae971ef7b3064834ec
erwanlr <erwan.lr@gmail.com>
2015-10-13 13:17:22 +0100

    Fixes #873 - mu-plugins detection

--- lib/wpscan/wp_target/wp_must_use_plugins.rb
+++ lib/wpscan/wp_target/wp_must_use_plugins.rb
@@ -2,14 +2,13 @@
 
 class WpTarget < WebSite
   module WpMustUsePlugins
-
     # Checks to see if the must use plugin folder exists
     #
     # @return [ Boolean ]
     def has_must_use_plugins?
       response = Browser.get(must_use_url)
 
-      if response && WpTarget.valid_response_codes.include?(response.code)
+      if response && [200, 401, 403].include?(response.code)
         hash = WebSite.page_hash(response)
         return true if hash != error_404_hash && hash != homepage_hash
       end
@@ -21,6 +20,5 @@ class WpTarget < WebSite
     def must_use_url
       @uri.merge("#{wp_content_dir}/mu-plugins/").to_s
     end
-
   end
 end
