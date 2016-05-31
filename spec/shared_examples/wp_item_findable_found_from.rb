72c2c1992b854e41a0ac84b12dade4bf8f655cb9
Christian Mehlmauer <firefart@gmail.com>
2016-05-31 15:23:34 +0200

    rspec fixed

--- spec/shared_examples/wp_item_findable_found_from.rb
+++ spec/shared_examples/wp_item_findable_found_from.rb
@@ -7,12 +7,6 @@ shared_examples 'WpItem::Findable#Found_From=' do
       subject.found_from = @method
       expect(subject.found_from).to eq @expected
     end
-    context 'when the pattern is not found' do
-      it 'returns nil' do
-        @method   = 'I_do_not_match'
-        @expected = nil
-      end
-    end
 
     it 'replaces _ by space' do
       @method   = 'find_from_some_detection_method'
