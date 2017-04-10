47ea7e27b355a6a67c52809cba5e8e4a3b9013fa
lejeun_j <jean.lejeune@bssi.fr>
2017-04-10 16:45:43 +0200

    Adding REST route enumeration

--- lib/common/collections/wp_users.rb
+++ lib/common/collections/wp_users.rb
@@ -3,9 +3,11 @@
 require 'common/collections/wp_users/detectable'
 require 'common/collections/wp_users/output'
 require 'common/collections/wp_users/brute_forcable'
+require 'common/collections/wp_users/RESTable'
 
 class WpUsers < WpItems
   extend WpUsers::Detectable
+  extend WpUsers::RESTable
   include WpUsers::Output
   include WpUsers::BruteForcable
 end
