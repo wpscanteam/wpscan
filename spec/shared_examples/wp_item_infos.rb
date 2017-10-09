2f5599c863748c005c3a500afcd313aa3f725927
Christian Mehlmauer <firefart@gmail.com>
2017-10-09 12:43:35 +0200

    specs

--- spec/shared_examples/wp_item_infos.rb
+++ spec/shared_examples/wp_item_infos.rb
@@ -16,7 +16,7 @@ shared_examples 'WpItem::Infos' do
     end
 
     context 'when the file exists' do
-      %w{readme.txt README.TXT}.each do |readme|
+      %w{readme.txt readme.md}.each do |readme|
         it 'returns the correct url' do
           url       = uri.merge(readme).to_s
           @expected = url
