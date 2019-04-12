64e469568b7cde591cb51a147f33ac7dc5e826b1
erwanlr <erwan.lr@gmail.com>
2019-04-12 14:07:28 +0100

    Adds message help for --wp-content-dir and --wp-plugins-dir options

--- app/controllers/custom_directories.rb
+++ app/controllers/custom_directories.rb
@@ -7,8 +7,10 @@ module WPScan
     class CustomDirectories < CMSScanner::Controller::Base
       def cli_options
         [
-          OptString.new(['--wp-content-dir DIR']),
-          OptString.new(['--wp-plugins-dir DIR'])
+          OptString.new(['--wp-content-dir DIR',
+                         'The wp-content directory if custom or not detected, such as "wp-content"']),
+          OptString.new(['--wp-plugins-dir DIR',
+                         'The plugins directory if custom or not deteced, such as "wp-content/plugins"'])
         ]
       end
 
