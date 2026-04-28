0c3bf351186a515a44894c860e3b6ce7cc493de1
Rafael Agostini <rafael.agostini@automattic.com>
2026-04-28 10:52:45 -0500

    Improve threshold error messages to mention threshold options

--- lib/wpscan/errors/enumeration.rb
+++ lib/wpscan/errors/enumeration.rb
@@ -5,16 +5,16 @@ module WPScan
     class PluginsThresholdReached < Standard
       def to_s
         "The number of plugins detected reached the threshold of #{ParsedCli.plugins_threshold} " \
-          'which might indicate False Positive. It would be recommended to use the --exclude-content-based ' \
-          'option to ignore the bad responses.'
+          'which might indicate False Positive. You can use --plugins-threshold to increase or disable ' \
+          'this limit (set to 0 to disable), or use --exclude-content-based to ignore bad responses.'
       end
     end
 
     class ThemesThresholdReached < Standard
       def to_s
         "The number of themes detected reached the threshold of #{ParsedCli.themes_threshold} " \
-          'which might indicate False Positive. It would be recommended to use the --exclude-content-based ' \
-          'option to ignore the bad responses.'
+          'which might indicate False Positive. You can use --themes-threshold to increase or disable ' \
+          'this limit (set to 0 to disable), or use --exclude-content-based to ignore bad responses.'
       end
     end
   end
