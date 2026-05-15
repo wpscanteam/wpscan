95ecf2c23df3ca9f2f38eebb1c0f6191e56dd6a7
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-14 23:49:58 -0500

    Implement Users::AuthorPosts#passive test

--- spec/app/finders/users/author_posts_spec.rb
+++ spec/app/finders/users/author_posts_spec.rb
@@ -7,7 +7,45 @@ describe WPScan::Finders::Users::AuthorPosts do
   let(:fixtures)   { FINDERS_FIXTURES.join('users', 'author_posts') }
 
   describe '#passive' do
-    xit
+    let(:homepage_body) { File.read(fixtures.join('potential_usernames.html')) }
+
+    before do
+      expect(target).to receive(:homepage_res).at_least(:once)
+                                              .and_return(Typhoeus::Response.new(body: homepage_body))
+      expect(target).to receive(:in_scope_uris).and_yield(
+        Addressable::URI.parse("#{url}author/admin/"),
+        double(text: '')
+      ).and_yield(
+        Addressable::URI.parse("#{url}?author=2"),
+        double(text: 'admin display_name')
+      ).and_yield(
+        Addressable::URI.parse("#{url}author/editor/"),
+        double(text: '')
+      ).and_yield(
+        Addressable::URI.parse("#{url}?author=3"),
+        double(text: 'editor')
+      )
+    end
+
+    it 'returns Users with expected usernames and confidence' do
+      users = finder.passive
+
+      expect(users.size).to eq 4
+      expect(users[0]).to be_a WPScan::Model::User
+      expect(users[0].username).to eq 'admin'
+      expect(users[0].confidence).to eq 100
+      expect(users[0].found_by).to match(/Author Posts.*Author Pattern/)
+
+      expect(users[1].username).to eq 'admin display_name'
+      expect(users[1].confidence).to eq 30
+      expect(users[1].found_by).to match(/Author Posts.*Display Name/)
+
+      expect(users[2].username).to eq 'editor'
+      expect(users[2].confidence).to eq 100
+
+      expect(users[3].username).to eq 'editor'
+      expect(users[3].confidence).to eq 30
+    end
   end
 
   describe '#potential_usernames' do
