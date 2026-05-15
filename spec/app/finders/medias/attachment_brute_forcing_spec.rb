c59122c15f8e22594b138d1a207a04e8a324abd2
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-15 00:03:34 -0500

    Implement Medias::AttachmentBruteForcing#aggressive test

--- spec/app/finders/medias/attachment_brute_forcing_spec.rb
+++ spec/app/finders/medias/attachment_brute_forcing_spec.rb
@@ -7,7 +7,34 @@ describe WPScan::Finders::Medias::AttachmentBruteForcing do
   let(:fixtures)   { FINDERS_FIXTURES.join('medias', 'attachment_brute_forcing') }
 
   describe '#aggressive' do
-    xit
+    let(:opts) { { range: (1..3) } }
+
+    before do
+      stub_request(:get, url).to_return(status: 200, body: '')
+      stub_request(:head, url).to_return(status: 200)
+
+      stub_request(:head, "#{url}?attachment_id=1").to_return(status: 200)
+      stub_request(:get, "#{url}?attachment_id=1").to_return(status: 200, body: '')
+
+      stub_request(:head, "#{url}?attachment_id=2").to_return(status: 200)
+      stub_request(:get, "#{url}?attachment_id=2").to_return(status: 200, body: '')
+
+      stub_request(:head, "#{url}?attachment_id=3").to_return(status: 404)
+      stub_request(:get, "#{url}?attachment_id=3").to_return(status: 404, body: '')
+    end
+
+    it 'returns Media objects for valid attachment IDs' do
+      medias = finder.aggressive(opts)
+
+      expect(medias.size).to eq 2
+      expect(medias[0]).to be_a WPScan::Model::Media
+      expect(medias[0].url).to eq "#{url}?attachment_id=1"
+      expect(medias[0].confidence).to eq 100
+      expect(medias[0].found_by).to match(/Attachment Brute Forcing/)
+
+      expect(medias[1].url).to eq "#{url}?attachment_id=2"
+      expect(medias[1].confidence).to eq 100
+    end
   end
 
   describe '#target_urls' do
