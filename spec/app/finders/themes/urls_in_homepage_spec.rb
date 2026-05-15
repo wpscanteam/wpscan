983eeac3531d5ce38a70b3aba82218ac1fd9cec7
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-15 00:06:42 -0500

    Implement Themes::UrlsInHomepage#passive test

--- spec/app/finders/themes/urls_in_homepage_spec.rb
+++ spec/app/finders/themes/urls_in_homepage_spec.rb
@@ -18,6 +18,31 @@ describe WPScan::Finders::Themes::UrlsInHomepage do
   end
 
   describe '#passive' do
-    xit
+    before do
+      stub_request(:get, finder.target.url)
+        .to_return(body: File.read(fixtures.join('found.html')))
+
+      expect(finder.target).to receive(:content_dir).at_least(1).and_return('wp-content')
+
+      stub_request(:get, 'http://wp.lab/wp-content/themes/dl-1/style.css')
+        .to_return(status: 200, body: '')
+      stub_request(:get, 'http://wp.lab/wp-content/themes/dc-1/style.css')
+        .to_return(status: 200, body: '')
+    end
+
+    it 'returns themes detected from links and code in the homepage' do
+      themes = finder.passive
+
+      expected_slugs = %w[dc-1 dl-1]
+
+      expect(themes.size).to eq 2
+      expect(themes.map(&:slug).sort).to eq expected_slugs.sort
+
+      themes.each do |theme|
+        expect(theme).to be_a WPScan::Model::Theme
+        expect(theme.confidence).to eq 80
+        expect(theme.found_by).to match(/Urls In Homepage/)
+      end
+    end
   end
 end
