dc7419845f24593d5b2f3dec093fbda9a4e27db0
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-15 09:46:08 -0500

    Implement Plugins view test

--- spec/shared_examples/views/enumeration/plugins.rb
+++ spec/shared_examples/views/enumeration/plugins.rb
@@ -14,7 +14,20 @@ shared_examples 'App::Views::Enumeration::Plugins' do
     end
 
     context 'when plugins found' do
-      xit
+      let(:p1) { plugin.new('test-plugin-1', target, found_by: 'Known Locations (Aggressive Detection)') }
+      let(:p2) { plugin.new('test-plugin-2', target, found_by: 'Urls In Homepage (Passive Detection)') }
+      let(:plugins) { [p1, p2] }
+      let(:expected_view) { File.join(view, 'plugins') }
+
+      before do
+        expect(target).to receive(:content_dir).at_least(1).and_return('wp-content')
+        stub_request(:head, /.*/)
+        stub_request(:get, /.*/)
+      end
+
+      it 'outputs the expected string' do
+        @tpl_vars = tpl_vars.merge(plugins: plugins)
+      end
     end
   end
 end
