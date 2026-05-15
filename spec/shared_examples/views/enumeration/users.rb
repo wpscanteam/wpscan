443ba41199aaf883babb10875886152604e560c1
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-15 09:38:14 -0500

    Implement Users view test

--- spec/shared_examples/views/enumeration/users.rb
+++ spec/shared_examples/views/enumeration/users.rb
@@ -14,9 +14,14 @@ shared_examples 'App::Views::Enumeration::Users' do
     end
 
     context 'when users found' do
+      let(:u1)     { user.new('admin', found_by: 'Author Id Brute Forcing (Aggressive Detection)') }
+      let(:u2)     { user.new('editor', found_by: 'Author Posts (Passive Detection)') }
+      let(:users)  { [u1, u2] }
       let(:expected_view) { File.join(view, 'users') }
 
-      xit 'outputs the expected string'
+      it 'outputs the expected string' do
+        @tpl_vars = tpl_vars.merge(users: users)
+      end
     end
   end
 end
