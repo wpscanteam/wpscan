9512329f2ff95fb4f1ee931c07285aff6603d243
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-15 09:40:27 -0500

    Implement ConfigBackups view test

--- spec/shared_examples/views/enumeration/config_backups.rb
+++ spec/shared_examples/views/enumeration/config_backups.rb
@@ -14,7 +14,14 @@ shared_examples 'App::Views::Enumeration::ConfigBackups' do
     end
 
     context 'when backups found' do
-      xit
+      let(:cb1) { config_backup.new("#{target_url}wp-config.php~", found_by: 'Direct Access (Aggressive Detection)') }
+      let(:cb2) { config_backup.new("#{target_url}wp-config.php.bak", found_by: 'Direct Access (Aggressive Detection)') }
+      let(:config_backups) { [cb1, cb2] }
+      let(:expected_view) { File.join(view, 'config_backups') }
+
+      it 'outputs the expected string' do
+        @tpl_vars = tpl_vars.merge(config_backups: config_backups)
+      end
     end
   end
 end
