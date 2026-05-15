d42bc895408ad3bd4a91aff6f976d6fed1061ee7
Rafael Agostini <rafael.agostini@automattic.com>
2026-05-14 22:20:50 -0500

    Implement TmmDbMigrate#aggressive test

--- spec/app/finders/interesting_findings/tmm_db_migrate_spec.rb
+++ spec/app/finders/interesting_findings/tmm_db_migrate_spec.rb
@@ -7,6 +7,64 @@ describe WPScan::Finders::InterestingFindings::TmmDbMigrate do
   let(:fixtures)   { FINDERS_FIXTURES.join('interesting_findings', 'tmm_db_migrate') }
 
   describe '#aggressive' do
-    xit
+    let(:zip_url) { 'http://ex.lo/wp-content/uploads/tmm_db_migrate/tmm_db_migrate.zip' }
+
+    before do
+      allow(target).to receive(:sub_dir).and_return(false)
+      allow(target).to receive(:content_dir).and_return('wp-content')
+      expect(target).to receive(:head_or_get_request_params).and_return(method: :head)
+      stub_request(:head, zip_url).to_return(status: status_code, headers: headers)
+    end
+
+    context 'when file returns 404' do
+      let(:status_code) { 404 }
+      let(:headers) { {} }
+
+      it 'returns nil' do
+        expect(finder.aggressive).to be nil
+      end
+    end
+
+    context 'when file returns 200 but wrong Content-Type' do
+      let(:status_code) { 200 }
+      let(:headers) { { 'Content-Type' => 'text/html' } }
+
+      it 'returns nil' do
+        expect(finder.aggressive).to be nil
+      end
+    end
+
+    context 'when file returns 200 with application/zip Content-Type' do
+      let(:status_code) { 200 }
+      let(:headers) { { 'Content-Type' => 'application/zip' } }
+
+      it 'returns TmmDbMigrate finding' do
+        result = finder.aggressive
+
+        expect(result).to be_a WPScan::Model::TmmDbMigrate
+        expect(result.url).to eq zip_url
+        expect(result.confidence).to eq 100
+        expect(result.found_by).to eq 'Direct Access (Aggressive Detection)'
+      end
+    end
+
+    context 'when file returns 200 with application/zip; charset=binary' do
+      let(:status_code) { 200 }
+      let(:headers) { { 'Content-Type' => 'application/zip; charset=binary' } }
+
+      it 'returns TmmDbMigrate finding' do
+        result = finder.aggressive
+        expect(result).to be_a WPScan::Model::TmmDbMigrate
+      end
+    end
+
+    context 'when file returns 200 but no Content-Type header' do
+      let(:status_code) { 200 }
+      let(:headers) { {} }
+
+      it 'returns nil' do
+        expect(finder.aggressive).to be nil
+      end
+    end
   end
 end
